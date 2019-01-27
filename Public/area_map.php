944fee873863ea6a32fbca349b59f62c5fb5d9af
reg <ex123789@gmail.com>
2019-01-27 17:12:33 +0800

    地圖功能與串連的php sample 0127

---
+++ Public/area_map.php
@@ -0,0 +1,14 @@
+<?php for($i = 0; $i < 5; $i++): ?>
+<div class="item">
+    <h4 class="card-title text-center r_noto_regular_p pt-2 mb-2">艾米利亞 羅馬涅大區</h4>
+    <h4 class="card-title text-center r_en_font_p pb-2">Emilia-Romagna</h4>
+    <img src="img/recipe/24_party_LAB.jpg" alt="">
+    <h5 class="card-title text-center r_en_font_p pt-4 mb-1">Lasagne alla Bolognese</h5>
+    <h5 class="card-title text-center r_noto_regular_p pb-2">波隆那肉醬千層麵</h5>
+    <h6 class="card-title text-justify r_noto_light_p pb-2">艾米利亞-羅馬涅大區人口約4百萬，人口密度為180人/平方公里，是義大利最發達的大區之一，亦是義大利著名的美食天堂。波隆那肉醬千層麵為該地經典的義式美食為著名卡通明星加菲貓的最愛。</h6>
+    <button type="button" class="btn r_btn col-md-12 mx-auto r_noto_regular_p pt-3 pb-3 mt-0">
+        前往食譜頁
+    </button>
+</div>
+<?php endfor;
+
---
+++ Public/id_map.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<!-- The Modal Head -->
+<div class="modal-header border-bottom-0 pb-0 ">
+    <div href="#" class="col-12 modal-title mx-auto text-center">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="card-title text-center r_noto_regular pt-4" id="title_ch">艾米利亞 羅馬涅大區</h4>
+        <h4 class="card-title text-center en_font" id="title_en">Emilia-Romagna</h4>
+    </div>
+</div>
+<!-- The Modal Body -->
+<div class="modal-body pt-0 pb-0 pr-5 pl-5">
+    <img src="img/recipe/24_party_LAB.jpg" alt="">
+    <h5 class="card-title text-center r_en_font pt-4 mb-1" id="district_en">Lasagne alla Bolognese</h5>
+    <h5 class="card-title text-center r_noto_light pb-3" id="district_ch">波隆那肉醬千層麵</h5>
+    <h6 class="card-title text-justify noto_light" id="district_des">
+        艾米利亞-羅馬涅大區人口約4百萬，人口密度為180人/平方公里，是義大利最發達的大區之一，亦是義大利著名的美食天堂。波隆那肉醬千層麵為該地經典的義式美食，為著名卡通明星加菲貓的最愛。</h6>
+    <div class="input-group mb-3">
+    </div>
+</div>
+<!-- The Modal Footer -->
+<div class="modal-footer pt-0 pr-5 pl-5 border-top-0">
+    <a href="./" class="btn r_btn col-md-12 mx-auto noto_light pt-3 pb-3 mt-0 mb-3">
+        前往食譜頁
+
+    </a>
+</div>
+
