570b8ec66b1f3b6d723bf0fd8a06490f192ba144
Elodie Lin <angelsanctuarydn@gmail.com>
2019-01-28 15:53:49 +0800

    0128 - past map update for desktop

--- Public/id_map.php
+++ Public/id_map.php
@@ -1,4 +1,16 @@
 <?php
+
+require __DIR__. '/__connect_db.php';
+
+$_GET['id'];
+
+
+$m_sql = sprintf("SELECT * FROM `pasta_map` WHERE `id` = '" . $_GET['id'] . "' ");
+$m_stmt = $pdo->query($m_sql);
+
+while ($row = $m_stmt->fetch(PDO::FETCH_ASSOC)) {
+    $map = $row;
+}
 ?>
 <!-- The Modal Head -->
 <div class="modal-header border-bottom-0 pb-0 ">
@@ -6,17 +18,19 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
-        <h4 class="card-title text-center r_noto_regular pt-4" id="title_ch">艾米利亞 羅馬涅大區</h4>
-        <h4 class="card-title text-center en_font" id="title_en">Emilia-Romagna</h4>
+        <h4 class="card-title text-center r_noto_regular pt-4" id=""><?= $map['District_Ch'] ?></h4>
+        <h4 class="card-title text-center en_font" id="title_en"><?= $map['District_En'] ?></h4>
+
     </div>
 </div>
 <!-- The Modal Body -->
 <div class="modal-body pt-0 pb-0 pr-5 pl-5">
-    <img src="img/recipe/24_party_LAB.jpg" alt="">
-    <h5 class="card-title text-center r_en_font pt-4 mb-1" id="district_en">Lasagne alla Bolognese</h5>
-    <h5 class="card-title text-center r_noto_light pb-3" id="district_ch">波隆那肉醬千層麵</h5>
+<img src="img/recipe/<?= $map['Image'] ?>.jpg" alt="">
+    <h5 class="card-title text-center r_en_font pt-4 mb-1" id="district_en"><?= $map['Recipe_name_En'] ?></h5>
+    <h5 class="card-title text-center r_noto_light pb-3" id="district_ch"><?= $map['Recipe_name_Ch'] ?></h5>
     <h6 class="card-title text-justify noto_light" id="district_des">
-        艾米利亞-羅馬涅大區人口約4百萬，人口密度為180人/平方公里，是義大利最發達的大區之一，亦是義大利著名的美食天堂。波隆那肉醬千層麵為該地經典的義式美食，為著名卡通明星加菲貓的最愛。</h6>
+    <?= $map['Description'] ?></h6>
+</h6>
     <div class="input-group mb-3">
     </div>
 </div>
