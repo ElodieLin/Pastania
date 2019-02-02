9cf2e4e73418338ea9bf5e7c90484dfdc842e2d5
Elodie Lin <angelsanctuarydn@gmail.com>
2019-02-02 20:17:09 +0800

    0202 - remove bootstrap link on head

--- Public/cart_step3.php
+++ Public/cart_step3.php
@@ -6,9 +6,9 @@ $_SESSION['order'] = $_POST['order'];
   <head>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-          crossorigin="anonymous">
+          crossorigin="anonymous"> -->
     <link rel="stylesheet" href="css/cart_step3.css">
 
   </head>
