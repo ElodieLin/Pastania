f541f4a5705de5b80ed6efacd62aecf3473d7fcf
reg <ex123789@gmail.com>
2019-02-15 15:12:47 +0800

    2019/02/15 編輯頁面樣式修改

--- Public/edit_me.php
+++ Public/edit_me.php
@@ -22,27 +22,47 @@ if (!isset($_SESSION['user'])) {
       margin-top: 100px;
     }
 
+    .color-false{
+      color: #D3554A;
+    }
+
+    .color-success{
+      color: #0062cc;
+    }
+
+    .card-text{
+      max-width: 600px;
+      margin: 0 auto;
+    }
+
+    .r_edit_bth{
+      width: 100%;
+      max-width: 600px;
+    }
+
+    @media screen and (max-width: 575.98px) {
+      .r_edit_bth{
+        max-width: 350px;
+      }
+    }
+
+
   </style>
-
-
 
 
   <div class="container col-lg-5">
     <div class="row ">
       <div class="col-lg-12">
 
-        <div id="info" class="alert" role="alert" style="display: none"></div>
 
         <div class="card">
           <div class="edit_logo mt-4 mb-2">
-            <a href="./" class="col-12 modal-title mx-auto text-center"><img src="img/icon/pastania_logo.svg"
-                                                                             class="logo_img" alt=""></a>
+            <h5 class="text-center">編輯個人資料</h5>
           </div>
           <hr class="col-lg-10 mx-auto">
+          <div id="info" class="text-center" role="alert" style="display: none"></div>
           <div class="card-body">
-            <h5 class="card-title text-center pb-3">編輯個人資料</h5>
             <div class="card-text">
-
               <form name="form3" method="post">
                 <div class="form-group row mx-auto">
                   <label for="email" class="col-md-3 label_text">電子郵箱</label>
@@ -51,7 +71,7 @@ if (!isset($_SESSION['user'])) {
                   <small id="email3Help" class="form-text"></small>
                 </div>
                 <div class="form-group row mx-auto">
-                  <label for="password" class="col-md-3 label_text">輸入密碼用以變更資料<span class="red">*</span></label>
+                  <label for="password" class="col-md-3 label_text">輸入密碼確認<span class="red">*</span></label>
                   <input type="password" class="form-control col-md-9" id="password4" name="password">
                   <small id="password4Help" class="form-text"></small>
                 </div>
@@ -70,10 +90,10 @@ if (!isset($_SESSION['user'])) {
                 <div class="form-group row mx-auto">
                   <label for="address" class="col-md-3 label_text">地址</label>
                   <!-- postcode select start  -->
-                  <div class="form-group col-md-9 row mx-auto zipcode">
+                  <div class="form-group col-md-9 row mx-auto zipcode mb-0" style="padding: 0">
 
                     <!-- postcode select start  -->
-                    <input type="text" class="form-control col-sm-3 mr-4 my-1" id="postcode"
+                    <input type="text" class="form-control col-sm-3 mr-4 my-1" id="postcode" readonly
                            placeholder="郵遞區號" name="postcode" value="<?= $_SESSION['user']['postcode'] ?>">
                     <!-- with selected city & district -->
 
@@ -88,7 +108,7 @@ if (!isset($_SESSION['user'])) {
                     <!-- postcode select end  -->
 
                   </div>
-<!---->
+                  <!---->
                 </div>
                 <div class="form-group row mx-auto">
                   <div class="col-lg-3 "></div>
@@ -96,8 +116,8 @@ if (!isset($_SESSION['user'])) {
                          value="<?= $_SESSION['user']['address'] ?>">
                 </div>
 
-                <div class="row d-flex justify-content-center">
-                  <button type="submit" class="btn btn-primary col-lg-10">修改</button>
+                <div class="row d-flex justify-content-center pt-3">
+                  <button type="submit" class="btn btn-primary r_edit_bth">修改</button>
                 </div>
               </form>
 
@@ -112,90 +132,79 @@ if (!isset($_SESSION['user'])) {
   <script>
 
 
-    $(function () {
-        var form = $('[name="form3"]'),
-            city = form.find('[name="city"]'),
-            area = form.find('[name="area"]'),
-            cityVal = '<?php echo $_SESSION['user']['city']; ?>',
-            areaVal = '<?php echo $_SESSION['user']['area']; ?>';
-        city.val(cityVal).change();
-        setTimeout(function () {
-            area.val(areaVal).change();
-        }, 10);
+      $(function () {
+          var form = $('[name="form3"]'),
+              city = form.find('[name="city"]'),
+              area = form.find('[name="area"]'),
+              cityVal = '<?php echo $_SESSION['user']['city']; ?>',
+              areaVal = '<?php echo $_SESSION['user']['area']; ?>';
+          city.val(cityVal).change();
+          setTimeout(function () {
+              area.val(areaVal).change();
+          }, 10);
 
-        form.submit(function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            formCheck();
-        });
+          form.submit(function (e) {
+              e.preventDefault();
+              e.stopPropagation();
+              formCheck();
+          });
 
-        var fields = ['nickname2', 'password4'];
-        var i, s;
-        var info = $('#info');
+          var fields = ['nickname2', 'password4'];
+          var i, s;
+          var info = $('#info');
 
-        function formCheck() {
-
-
-            info.hide();
-            // 讓每一欄都恢復原來的狀態
-            for (s in fields) {
-                cancelAlert(fields[s]);
-            }
-
-            var isPass = true;
+          function formCheck() {
 
 
-            if (document.form3.nickname.value.length < 2) {
-                setAlert('nickname2', '請輸入正確的暱稱');
-                isPass = false;
-            }
+              info.hide();
+              // 讓每一欄都恢復原來的狀態
+              for (s in fields) {
+                  cancelAlert(fields[s]);
+              }
+
+              var isPass = true;
 
 
-
-            if (document.form3.password.value.length < 6) {
-                setAlert('password4', '密碼請輸入六個字以上');
-                isPass = false;
-            }
-            if (isPass) {
+              if (isPass) {
 
 
-                $.post('edit_me_api.php', $(document.form3).serialize(), function (data) {
-                    var alertType = 'alert-danger';
+                  $.post('edit_me_api.php', $(document.form3).serialize(), function (data) {
+                      var alertType = 'color-false';
 
-                    info.removeClass('alert-danger');
-                    info.removeClass('alert-success');
+                      info.removeClass('color-false');
+                      info.removeClass('color-success');
 
-                    if (data.success) {
-                        alertType = 'alert-success';
-                        form.find('[name="password"]').val('');
-                    } else {
-                        alertType = 'alert-danger';
-                    }
-                    info.addClass(alertType);
-                    if (data.info) {
-                        info.html(data.info);
-                        info.slideDown();
-                    }
-                }, 'json');
+                      if (data.success) {
+                          alertType = 'color-success';
+                          form.find('[name="password"]').val('');
+                      } else {
+                          alertType = 'color-false';
+                      }
+                      info.addClass(alertType);
+                      if (data.info) {
+                          info.html(data.info);
+                          info.slideDown();
+                      }
+                  }, 'json');
 
-            }
+              }
 
-            return false;
-        }
+              return false;
+          }
 
-        // 設定警示
-        function setAlert(fieldName, msg) {
-            $('#' + fieldName).css('border', '1px solid red');
-            $('#' + fieldName + 'Help').text(msg);
-        }
+          // 設定警示
+          function setAlert(fieldName, msg) {
+              $('#' + fieldName).css('border', '1px solid red');
+              $('#' + fieldName + 'Help').text(msg);
+          }
 
-        // 取消警示
-        function cancelAlert(fieldName) {
-            $('#' + fieldName).css('border', '1px solid #cccccc');
-            $('#' + fieldName + 'Help').text('');
-        }
+          // 取消警示
+          function cancelAlert(fieldName) {
+              $('#' + fieldName).css('border', '1px solid #cccccc');
+              $('#' + fieldName + 'Help').text('');
+          }
 
-    });
+      });
 
 
   </script>
